dd99e717ccb6f07134ec41eebe7091f19fd8de4e
lab-machine <ahasanhabib51971@gmail.com>
2025-07-09 12:55:11 +0600

    added mainlayout

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+import { Outlet } from 'react-router';
+import Footer from '../components/Footer/Footer';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+            <Footer />
+            
+        </div>
+    );
+};
+
+export default MainLayout;
