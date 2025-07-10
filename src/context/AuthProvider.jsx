43ec8da3d509d470678c9f39053b3029476d2453
lab-machine <ahasanhabib51971@gmail.com>
2025-07-10 06:23:13 +0600

    Implemented sign with email and pass word , sign with google and set observer

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { auth } from '../config/firebase.config';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
+// import axiosInstance from '../api/axiosInstance';
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    //Sign in user with email and password
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password)
+            .finally(() => {
+                setLoading(false);
+            });
+    }
+
+    //Google sign in
+    const signInWithGoogle = () => {
+        setLoading(true);
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider)
+            .finally(() => {
+                setLoading(false);
+            });
+    
+    };
+
+    // Set user state when auth state changes
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            currentUser? console.log('Current user from observer:', currentUser): console.log('No user is signed in');
+            setUser(currentUser);
+            setLoading(false);
+            // if(currentUser?.email){
+            //    const userData = { email: currentUser.email }
+            //     axiosInstance.post('/jwt', userData, {
+            //         withCredentials: true
+            //     })
+            //     .then(res => {
+            //         console.log('token after jwt', res.data);
+            //     })
+            //     .catch(err => {
+            //         console.error('Error fetching JWT token:', err);
+            //     });
+
+            // }
+
+        });
+        return () => unsubscribe();
+    }, []);
 
     const authInfo = {
-        name: "John Doe",
+        user,
+        loading,
+        createUser,
+        setUser,
+        signInUser,
+        signInWithGoogle,
         
     }
 
