6691fdd83e27e6d6449a6ca323193c41d62c38c3
lab-machine <ahasanhabib51971@gmail.com>
2025-07-11 23:33:29 +0600

    login route dashboard added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,6 +4,9 @@ import Home from "../pages/Home/Home";
 import NotFound from "../pages/NotFound";
 import Membership from "../pages/Membership/Membership";
 import Register from "../pages/Auth/Register";
+import Login from "../pages/Auth/Login";
+import DashBoard from "../pages/Dashboard/DashBoard";
+import PrivateRoute from "./PrivateRoute";
 
 
 const router = createBrowserRouter([
@@ -18,10 +21,19 @@ const router = createBrowserRouter([
             {
                 path:"membership",
                 Component: Membership,
+                // element:<PrivateRoute><Membership /></PrivateRoute>,
             },
             {
                 path:"register",
                 Component: Register,
+            },
+            {
+                path: "join-us",
+                Component: Login,
+            },
+            {
+                path:"dashboard",
+                Component:DashBoard,
             }
 
         ]
